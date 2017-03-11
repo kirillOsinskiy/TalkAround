4afb3b3ae51d32719616ad3b8ae6ed9085461b26
Kirill Osinskiy <Kirill.Osinsky@firstlinesoftware.com>
2017-03-11 19:40:51 +0300

    Update model

--- TalkAroundServer/src/main/java/com/osk/talkaround/model/CustomLocation.java
+++ TalkAroundServer/src/main/java/com/osk/talkaround/model/CustomLocation.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
 @XmlRootElement
 public class CustomLocation implements Serializable {
 
+    private static final long serialVersionUID = 207967188772438351L;
     private double longitude;
     private double latitude;
 
